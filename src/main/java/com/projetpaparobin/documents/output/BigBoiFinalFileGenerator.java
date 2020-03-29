e645a15304cdac62b2a79ee98ebf638df2f74297
JackAlgera <jack.algera@live.com>
2020-03-30 00:33:39 +0200

    Added POI maven dependancy

--- src/main/java/com/projetpaparobin/documents/output/BigBoiFinalFileGenerator.java
+++ src/main/java/com/projetpaparobin/documents/output/BigBoiFinalFileGenerator.java
@@ -8,4 +8,17 @@ public class BigBoiFinalFileGenerator {
 	private static LayoutHandler layoutHandler = LayoutHandler.getInstance();
 	private static DAOExcelImpl daoExcel = DAOExcelImpl.getInstance();
 	
+	private static BigBoiFinalFileGenerator instance;
+	
+	private BigBoiFinalFileGenerator() {	
+	}
+	
+	public static BigBoiFinalFileGenerator getInstance() {
+		if(instance == null) {
+			instance = new BigBoiFinalFileGenerator();
+		}
+		
+		return instance;
+	}
+		
 }
