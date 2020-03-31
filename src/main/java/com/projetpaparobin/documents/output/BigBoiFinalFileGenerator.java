66bd7b10ad46b6f74f2d23c11a8ac298e9934b26
Robinvi <robinvi@users.noreply.github.com>
2020-04-01 00:41:21 +0200

    Added writing in excel functionnality

--- src/main/java/com/projetpaparobin/documents/output/BigBoiFinalFileGenerator.java
+++ src/main/java/com/projetpaparobin/documents/output/BigBoiFinalFileGenerator.java
@@ -1,5 +1,17 @@
 package com.projetpaparobin.documents.output;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
 import com.projetpaparobin.documents.LayoutHandler;
 import com.projetpaparobin.documents.dao.DAOExcelImpl;
 
@@ -11,6 +23,8 @@ public class BigBoiFinalFileGenerator {
 	private static BigBoiFinalFileGenerator instance;
 	
 	private BigBoiFinalFileGenerator() {	
+		
+		
 	}
 	
 	public static BigBoiFinalFileGenerator getInstance() {
@@ -21,4 +35,26 @@ public class BigBoiFinalFileGenerator {
 		return instance;
 	}
 		
+	public void PutInExcel(String excelFilePath, int columnPos, int rowPos, int sheetNumber, String dataToAdd) {
+		try {
+			FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
+		    Workbook workbook = WorkbookFactory.create(inputStream);
+		    Sheet sheet = workbook.getSheetAt(sheetNumber);	
+		    Row row = sheet.getRow(rowPos);
+		    Cell cell = row.getCell(columnPos);
+		    if (dataToAdd instanceof String) {
+		        cell.setCellValue((String) dataToAdd);
+		    } 
+		    inputStream.close();
+		    FileOutputStream outputStream = new FileOutputStream("testmethod.xlsx");
+		    workbook.write(outputStream);
+		    workbook.close();
+		    outputStream.close();
+		}
+		catch (IOException | EncryptedDocumentException
+	            ex) {
+			System.out.println("ERROR");
+			// TODO: better handle of exception
+		}
+	}
 }
