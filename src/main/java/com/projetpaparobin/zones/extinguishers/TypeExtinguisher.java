f9355290809c466bd9f21075f7003ebd4c5264fb
JackAlgera <jack.algera@live.com>
2020-04-02 19:49:46 +0200

    Added extinguisher type to easily fill excel page

---
+++ src/main/java/com/projetpaparobin/zones/extinguishers/TypeExtinguisher.java
@@ -0,0 +1,49 @@
+package com.projetpaparobin.zones.extinguishers;
+
+public class TypeExtinguisher {
+
+	private String type;
+	private int fabricationYear;
+	
+	public TypeExtinguisher(String type, int fabricationYear) {
+		this.type = type;
+		this.fabricationYear = fabricationYear;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public int getFabricationYear() {
+		return fabricationYear;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + fabricationYear;
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TypeExtinguisher other = (TypeExtinguisher) obj;
+		if (fabricationYear != other.fabricationYear)
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}	
+
+}
